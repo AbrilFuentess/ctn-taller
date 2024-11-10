0e6ebaa9385cbe56b516bfe39a73fc13d2b02366
mielnasantarelli <milenitasantarelli@gmail.com>
2024-11-10 16:42:12 -0300

    pantalla de inicio conectada con login y registro

---
+++ pantallas/inicio.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/inicio.css">
+    <title>Inicio</title>
+</head>
+<body>
+    <div class="cont1">
+        <div class="uno">
+            <h1>CINE</h1>
+            <h2>NEUQUINO</h2>
+            <h3>TU CINE</h3>
+            <p>INCIA SESIÓN PARA ENCONTRAR TU PELÍCULA Y QUE NADIE TE SAQUE EL LUGAR</p>
+        </div>
+    </div>
+    <div class="cont2">
+        <div class="dos">
+            <a href="login_usuario.php">Logueate</a>
+            <a href="registro.php">Registrate</a>
+            <a href="acceso_personal.php">Acceso administrador</a>
+        </div>
+    </div>
+</body>
+</html>
